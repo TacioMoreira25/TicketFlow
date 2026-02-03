77fdd45fb3774951eaa58f1d20a46d181f0781f7
Seu Nome <seu_email@exemplo.com>
2026-02-03 20:55:36 -0300

    Initialize RabbitMQ service in CI workflow and set default values for event properties

--- TicketFlow.Domain/Entities/Event.cs
+++ TicketFlow.Domain/Entities/Event.cs
@@ -3,9 +3,9 @@ namespace TicketFlow.Domain.Entities;
 public class Event
 {
     public Guid Id { get; private set; }
-    public string Title { get; private set; }
+    public string Title { get; private set; } = string.Empty;
     public DateTime Date { get; private set; }
-    public string Description { get; private set; }
+    public string Description { get; private set; } = string.Empty;
 
     // Relação 1:N
     public ICollection<Ticket> Tickets { get; private set; } = new List<Ticket>();
